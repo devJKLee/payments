91fb771cb5a144716ae85681fb09340d15e5cd92
devJKLee <beneciajk@gmail.com>
2019-11-13 09:32:51 +0900

    프로젝트 초기 설정

---
+++ src/Server.js
@@ -0,0 +1,23 @@
+"use strict";
+/**
+ * Created by JK
+ * date: 2019-11-13
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var Server = /** @class */ (function () {
+    function Server() {
+    }
+    /**
+     * 서버 초기 설정
+     */
+    Server.prototype.init = function () {
+    };
+    /**
+     * 서버 시작
+     */
+    Server.prototype.serverStart = function () {
+    };
+    return Server;
+}());
+exports.Server = Server;
+var server = new Server();
---
+++ src/Server.ts
@@ -0,0 +1,34 @@
+/**
+ * Created by JK
+ * date: 2019-11-13
+ */
+
+import * as express from 'express';
+
+export class Server
+{
+    public static app:express.Application;
+
+    constructor()
+    {
+
+    }
+
+    /**
+     * 서버 초기 설정
+     */
+    private init():void
+    {
+        
+    }
+
+    /**
+     * 서버 시작
+     */
+    private serverStart():void
+    {
+        
+    }
+}
+
+let server:Server = new Server();
